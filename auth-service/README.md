b76644ad300f0faa12120baacfbbbd0e5c1a8e57
jaiybiyo <jbo140403@stud.gibb.ch>
2025-02-25 16:05:56 +0100

    initial commit

--- auth-service/README.md
+++ auth-service/README.md
@@ -1,4 +1,4 @@
 # Installation of all dependencies
-> `npm install express typescript @types/express @types/node @types/passport passport passport-google-oauth20 jsonwebtoken dotenv mariadb`
+> `npm install express express-session typescript @types/express @types/express-session @types/node @types/passport @types/passport-google-oauth20 @types/jsonwebtoken passport passport-google-oauth20 jsonwebtoken dotenv mariadb`
  
 > `npm install --save-dev nodemon ts-node`
--- auth-service/src/server.ts
+++ auth-service/src/server.ts
@@ -1,16 +1,64 @@
-import express from 'express';
 import dotenv from 'dotenv';
-import authRoutes from './routes/authRoutes'
+import express from 'express';
+import session from 'express-session';
+import passport from 'passport';
+import './utils/passport';
 
+// TODO: fix env import
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const SESSION_SECRET = process.env.SESSION_SECRET!;
 
+/**
+ * Middleware to access json data
+ */
 app.use(express.json());
-app.use('/auth', authRoutes);
+app.use(
+    session({
+        secret: SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+    })
+);
 
-app.listen(PORT, () => {
-    console.log(`Auth Service Running on Port ${PORT}`);
-});
+app.use(passport.initialize());
+app.use(passport.session());
+
+/**
+ * Login Route
+ */
+app.get('/', (req, res) => {
+    res.send('<a href="auth/google">Login with Google</a>');
+})
+
+/**
+ * Callback Route
+ */
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        accessType: 'offline',
+        scope: ['profile', 'email']
+    }),
+    (req, res) => {
+        if (!req.user) {
+            res.status(400).json({error: "Authentication Failed"});
+        }
+        res.status(200).json(req.user);
+    }
+);
+
+const start = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Auth Service Running on Port ${PORT}`);
+        });
+    } catch (error) {
+        console.log("Failed to Start Auth Service with Error: ", error);
+    }
+}
+
+start().then(res => {}).catch(error => {});
 
