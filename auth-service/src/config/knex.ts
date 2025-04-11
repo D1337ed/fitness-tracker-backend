96181db3e7213b9b6ca293513979ba938fe4eea0
jaiybiyo <jbo140403@stud.gibb.ch>
2025-04-11 10:49:11 +0200

    wip

--- auth-service/src/config/knex.ts
+++
@@ -1,7 +0,0 @@
-import knex from 'knex';
-// TODO: fix path or module
-import config from './knexfile';
-
-const db = knex(config);
-
-export default db;
--- auth-service/src/models/User/user.queries.ts
+++ auth-service/src/models/User/user.queries.ts
@@ -1,20 +1,5 @@
-import db from '../../config/knex';
-import {User, UserSignUp} from './user.types';
-
 export const findUserByEmail = async (
     email: string
-): Promise<User | undefined> => {
-    return db<User>('users')
-        .where('email', email)
-        .first();
-};
+)=> {};
 
-export const createUser = async (
-    userData: UserSignUp
-): Promise<User> => {
-    const [newUser] = await db<User>('users')
-        .insert(userData)
-        .returning('*');
-
-    return newUser;
-}
+export const createUser = async () => {};
