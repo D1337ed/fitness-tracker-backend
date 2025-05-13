e0307f37f7d37de93beb9721ea0b26efc6fa5b2b
D1337ed <jbo140403@stud.gibb.ch>
2025-05-13 13:50:14 +0200

    small clean up of unused and non-necessary things for auth database

--- calorie-service/src/server.ts
+++ calorie-service/src/server.ts
@@ -1,15 +1,1 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import calorieRoutes from './routes/calorieRoutes';
 
-dotenv.config();
-
-const app = express();
-const PORT = process.env.PORT || 4000;
-
-app.use(express.json());
-app.use('/calculate', calorieRoutes);
-
-app.listen(PORT, () => {
-    console.log(`Calorie Service Running on Port ${PORT}`);
-});
